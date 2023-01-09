a0422876bd0efca074604537c39536231ab80619
SanjotD <112981867+SanjotD@users.noreply.github.com>
2023-01-09 14:45:28 -0700

    gh

---
+++ main.js
@@ -0,0 +1,29 @@
+
+
+// document.getElementById("startGamebtn").addEventListener("click", playGame);
+
+//Variables
+
+let optRockEl = document.getElementById("optRock");
+let optPaperEl = document.getElementById("optPaper");
+let optScissorEl = document.getElementById("optScissor");
+
+
+//Event Listeners
+
+optRockEl.addEventListener("click", chooseRock);
+optPaperEl.addEventListener("click", choosePaper);
+optScissorEl.addEventListener("click", chooseScissor);
+
+
+function chooseRock() {
+
+}
+
+function choosePaper() {
+
+}
+
+function chooseScissor() {
+
+}
